1783d3e3870c25734194347b70428ad72f570bb4
wintyo <wintyo1024@gmail.com>
2020-02-15 22:41:38 +0900

    設定から許可するIPを指定して、アクセスできるようにした

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,34 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import { IpFilter } from 'express-ipfilter';
+import * as requestIp from 'request-ip';
 // import * as fs from 'fs';
 // import * as path from 'path';
 
-const PERMIT_IPS = [
-  '127.0.0.1',
-];
+// アクセス許可するIPの設定
+const PERMIT_IPS = (() => {
+  const config = functions.config();
+  const permitIps = (config.server && config.server['permit-ips']) || [];
+  return [
+    '127.0.0.1',
+  ].concat(...permitIps);
+})();
+
+console.log('PERMIT_IPS:', PERMIT_IPS);
 
 const app = express();
 
 app.use(IpFilter(PERMIT_IPS, {
   mode: 'allow',
   detectIp: (req) => {
+    const clientIp = requestIp.getClientIp(req);
     // IPが取れない場合はローカルIPにフォールバック
-    return req.connection.remoteAddress || '127.0.0.1';
+    return clientIp || '127.0.0.1';
   },
 }));
 
 app.get('/', (req, res) => {
-  res.send('key!');
+  res.send('ok');
 });
 
 export const server = functions.https.onRequest(app);
